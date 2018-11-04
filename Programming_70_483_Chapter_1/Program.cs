5ca7edac3cd1c28cb863af16d5834da1154387a3
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-04 19:04:55 +0100

    - Preliminary implementation of ConsoleTools.GetSpecificExercisesToRun() - Added pseudo code for next time

--- Programming_70_483_Chapter_1/Program.cs
+++ Programming_70_483_Chapter_1/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TommyTools;
 
 namespace Programming_70_483_Chapter_1
 {
@@ -21,6 +22,11 @@ namespace Programming_70_483_Chapter_1
                 Console.WriteLine(exercise.ToString());
                 //exercise.Exec();
             }
+
+            Console.WriteLine("Please enter the exercises you wish to run...");
+            int[] _specificExercisesToRun = ConsoleTools.GetSpecificExercisesToRun(); // Moet in eigen interface + UnitTests
+
+            // Dan volgt, op zelfde interface, een uitvoerder (+ UnitTests), die _specificExercisesToRun binnenkrijgt als parameter en de nodige oefeningen uitvoert
         }
     }
 }
