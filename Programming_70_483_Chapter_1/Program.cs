3d1a83bee92a1719675c407eae80568b45e6ba6c
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-14 18:12:02 +0100

    Now works as it should :-) - Array needed to be casted properly - Contents of array needed to be casted properly - Deleted comments - Deleted unused usings

--- Programming_70_483_Chapter_1/Program.cs
+++ Programming_70_483_Chapter_1/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Reflection;
 using TommyTools;
 
 namespace Programming_70_483_Chapter_1
@@ -13,31 +11,17 @@ namespace Programming_70_483_Chapter_1
             string nmspace = "Programming_70_483_Chapter_1";
             string startsWith = "Ex";
 
-            BaseExercise[] exercises = new BaseExercise[0];
-            exercises = consoleTools.GetObjectArrayOfClasses(nmspace, startsWith) as BaseExercise[];
-            /*
-            var exercisesArray = from target in Assembly.GetExecutingAssembly().GetTypes()
-                                 where target.IsClass && target.Namespace == nmspace && target.Name.StartsWith("Ex0")
-                                 orderby target.Name
-                                 select target;
-            exercisesArray.ToArray();
-
-            BaseExercise[] exercises = new BaseExercise[exercisesArray.Count()];
-            BaseExercise myExercise;
+            var classes = consoleTools.GetObjectArrayOfClasses(nmspace, startsWith);
+            BaseExercise[] exercises = new BaseExercise[classes.Length];
             int index = 0;
-
-            foreach (var clss in exercisesArray)
-            {
-                myExercise = Activator.CreateInstance(clss) as BaseExercise;
-                exercises[index] = myExercise;
+            foreach (var clss in classes)
+            {                
+                exercises[index] = clss as BaseExercise;
                 index++;
-                Console.WriteLine(myExercise.ToString());
             }
-            Console.WriteLine();
-            */
+
             int[] exercisesToRun = new int[0];
             bool isNumber;
-
             do
             {
                 Console.WriteLine("Please enter the exercise you wish to run... (0 is not valid) ");
@@ -51,16 +35,9 @@ namespace Programming_70_483_Chapter_1
             while (isNumber);
             Console.WriteLine();
 
-            try
+            foreach (BaseExercise exercise in exercises)
             {
-                foreach (BaseExercise exercise in exercises)
-                {
-                    exercise.NeedsExecution(exercisesToRun);
-                }
-            }
-            catch (NullReferenceException e)
-            {
-                Console.WriteLine("Oeps... {0}", e.ToString());
+                exercise.NeedsExecution(exercisesToRun);
             }
         }
     }
