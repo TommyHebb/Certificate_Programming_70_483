886a4dd7e4c3876eeb24f9ab247fd3bdaf6993dd
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-14 11:12:49 +0100

    Delete unused usings

--- Programming_70_483_Chapter_1/Exercise classes/Exercise.cs
+++ Programming_70_483_Chapter_1/Exercise classes/Exercise.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Programming_70_483_Chapter_1.Exercise_classes
 {
