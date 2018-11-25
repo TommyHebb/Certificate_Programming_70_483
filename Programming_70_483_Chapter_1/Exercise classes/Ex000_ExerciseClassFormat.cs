330e31fda68fe674a14c15a9c45b9b031d778541
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-25 17:38:02 +0100

    Added Ex013

---
+++ Programming_70_483_Chapter_1/Exercise classes/Ex000_ExerciseClassFormat.cs
@@ -0,0 +1,25 @@
+﻿/*
+using System;
+
+namespace Programming_70_483_Chapter_1
+{
+    class Ex000_ExerciseClassFormat : BaseExercise
+    {
+        public override string Name { get; set; } = "";
+
+        public override int Number { get; set; } = ;
+
+        public () { }
+
+        public override void Exec()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Oefening: {0} - {1} ", Number, Name);
+        }
+    }
+}
+*/
