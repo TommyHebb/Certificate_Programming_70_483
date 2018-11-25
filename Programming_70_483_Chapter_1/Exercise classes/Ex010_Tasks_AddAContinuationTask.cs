16f968e4887124286a461874d6470404f004cce3
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-25 17:24:38 +0100

    Added Ex010

---
+++ Programming_70_483_Chapter_1/Exercise classes/Ex010_Tasks_AddAContinuationTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Programming_70_483_Chapter_1
+{
+    class Ex010_Tasks_AddAContinuationTask : BaseExercise
+    {
+        public override string Name { get; set; } = "Tasks - Add a continuation Task";
+
+        public override int Number { get; set; } = 10;
+
+        public Ex010_Tasks_AddAContinuationTask() { }
+
+        public override void Exec()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2;
+            });
+            Console.WriteLine(t.Result);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Oefening: {0} - {1} ", Number, Name);
+        }
+    }
+}
