c02076b3d79b94caf46dd5ace3bcd1f164a44c29
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-25 17:59:01 +0100

    Added Ex020

---
+++ Programming_70_483_Chapter_1/Exercise classes/Ex020_AsyncAndAwait_PLINQ_UnorderedParallelQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Programming_70_483_Chapter_1
+{
+    class Ex020_AsyncAndAwait_PLINQ_UnorderedParallelQuery : BaseExercise
+    {
+        public override string Name { get; set; } = "Async and Await - PLINQ - Unordered parallel query";
+
+        public override int Number { get; set; } = 20;
+
+        public Ex020_AsyncAndAwait_PLINQ_UnorderedParallelQuery() { }
+
+        public override void Exec()
+        {
+            var numbers = Enumerable.Range(0, 10);
+            var parallelResult = numbers.AsParallel()
+                                        .Where(i => i % 2 == 0)
+                                        .ToArray();
+            foreach (int i in parallelResult)
+                Console.WriteLine(i);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Oefening: {0} - {1} ", Number, Name);
+        }
+    }
+}
