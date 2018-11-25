9f2622ff73fd6ba95833846013bd121e7d2cf84e
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-25 17:22:11 +0100

    Added Ex009

---
+++ Programming_70_483_Chapter_1/Exercise classes/Ex009_Tasks_IfATaskShouldReturnAValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Programming_70_483_Chapter_1
+{
+    class Ex009_Tasks_IfATaskShouldReturnAValue : BaseExercise
+    {
+        public override string Name { get; set; } = "Tasks - If a Task should return a value";
+
+        public override int Number { get; set; } = 9;
+
+        public Ex009_Tasks_IfATaskShouldReturnAValue() { }
+
+        public override void Exec()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+            });
+
+            Console.WriteLine(t.Result);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Oefening: {0} - {1} ", Number, Name);
+        }
+    }
+}
