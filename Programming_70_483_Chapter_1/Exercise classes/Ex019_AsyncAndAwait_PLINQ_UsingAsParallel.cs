d454c33481d5446aeca73817dfd93c9e6a312bb1
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-25 17:56:47 +0100

    Added Ex019

---
+++ Programming_70_483_Chapter_1/Exercise classes/Ex019_AsyncAndAwait_PLINQ_UsingAsParallel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Programming_70_483_Chapter_1
+{
+    class Ex019_AsyncAndAwait_PLINQ_UsingAsParallel : BaseExercise
+    {
+        public override string Name { get; set; } = "Async and Await - PLINQ - Using AsParallel";
+
+        public override int Number { get; set; } = 19;
+
+        public Ex019_AsyncAndAwait_PLINQ_UsingAsParallel() { }
+
+        public override void Exec()
+        {
+            var numbers = Enumerable.Range(0, 100000000);
+            var parallelResult = numbers.AsParallel()
+                                        .Where(i => i % 2 == 0)
+                                        .ToArray();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Oefening: {0} - {1} ", Number, Name);
+        }
+    }
+}
