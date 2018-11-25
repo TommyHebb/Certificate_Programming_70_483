cc4cec344e4a001b537940312cd9ec2abef127e5
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-25 17:49:29 +0100

    Added Ex017

---
+++ Programming_70_483_Chapter_1/Exercise classes/Ex017_Parallel_UsingParallelBreak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Programming_70_483_Chapter_1
+{
+    class Ex017_Parallel_UsingParallelBreak : BaseExercise
+    {
+        public override string Name { get; set; } = "Parallel - Using Parallel.Break";
+
+        public override int Number { get; set; } = 17;
+
+        public Ex017_Parallel_UsingParallelBreak() { }
+
+        public override void Exec()
+        {
+            ParallelLoopResult result = Parallel.For(0, 1000, (int i, ParallelLoopState loopState) =>
+            {
+                if (i == 500)
+                {
+                    Console.WriteLine("Breaking loop");
+                    loopState.Break();
+                }
+                return;
+            });
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Oefening: {0} - {1} ", Number, Name);
+        }
+    }
+}
