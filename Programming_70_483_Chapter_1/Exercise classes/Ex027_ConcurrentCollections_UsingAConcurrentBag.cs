60dd09ea3f37532b1882aa95c379fb11ef18a499
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-28 16:13:48 +0100

    - Added Ex027 - Added Ex028

---
+++ Programming_70_483_Chapter_1/Exercise classes/Ex027_ConcurrentCollections_UsingAConcurrentBag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using TommyTools;
+
+namespace Programming_70_483_Chapter_1
+{
+    class Ex027_ConcurrentCollections_UsingAConcurrentBag : BaseExercise
+    {
+        public override string Name { get; set; } = "Concurrent Collections - Using a ConcurrentBag<T>";
+
+        public override int Number { get; set; } = 27;
+
+        public Ex027_ConcurrentCollections_UsingAConcurrentBag() { }
+
+        public override void Exec()
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            bag.Add(42);
+            bag.Add(21);
+            int result;
+            if (bag.TryTake(out result))
+            {
+                Console.WriteLine(result);
+            }
+            if (bag.TryPeek(out result))
+            {
+                Console.WriteLine("There is a next item: {0}", result);
+            }
+        }
+    }
+}
---
+++ Programming_70_483_Chapter_1/Exercise classes/Ex028_ConcurrentCollections_EnumeratingAConcurrentBag.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using TommyTools;
+
+namespace Programming_70_483_Chapter_1
+{
+    class Ex028_ConcurrentCollections_EnumeratingAConcurrentBag : BaseExercise
+    {
+        public override string Name { get; set; } = "Concurrent Collections - Enumerating a ConcurrentBag<T>";
+
+        public override int Number { get; set; } = 28;
+
+        public Ex028_ConcurrentCollections_EnumeratingAConcurrentBag() { }
+
+        public override void Exec()
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            Task.Run(() =>
+            {
+                bag.Add(42);
+                Thread.Sleep(1000);
+                bag.Add(21);
+            });
+            Task.Run(() =>
+            {
+                foreach (int i in bag)
+                {
+                    Console.WriteLine(i);
+                }
+            }).Wait();
+        }
+    }
+}
