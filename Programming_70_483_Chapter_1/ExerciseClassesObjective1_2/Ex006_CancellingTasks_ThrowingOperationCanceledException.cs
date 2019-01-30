f67904e892cb58e1fcb83f373066055eb97ef3ac
Tommy Hebb <tommyhebb@hotmail.com>
2019-01-30 12:04:07 +0100

    Added Ex006 to Obj 1_2

---
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_2/Ex006_CancellingTasks_ThrowingOperationCanceledException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TommyTools;
+
+namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_2
+{
+    class Ex006_CancellingTasks_ThrowingOperationCanceledException : BaseExercise
+    {
+        public override string Name { get; set; } = "CancellingTasks - Throwing an OperationCanceledException";
+
+        public override int Number { get; set; } = 6;
+
+        public Ex006_CancellingTasks_ThrowingOperationCanceledException() { }
+
+        public override void Exec()
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(1000);
+                }
+                token.ThrowIfCancellationRequested();
+            }, token);
+            try
+            {
+                Console.WriteLine("Press enter to stop the task");
+                Console.ReadLine();
+                cancellationTokenSource.Cancel();
+                task.Wait();
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e.InnerExceptions[0].Message);
+            }
+            Console.WriteLine("Press enter to end the application");
+            Console.ReadLine();
+        }
+    }
+}
