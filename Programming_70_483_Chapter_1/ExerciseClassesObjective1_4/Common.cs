23eabb9084014ab5ef5118452d24d14bb3338694
Tommy Hebb <tommyhebb@hotmail.com>
2019-02-13 11:49:10 +0100

    - Added Common to Ch 1 Obj 4 - Added Ex001 to Ch 1 Obj 4

---
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Common.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TommyTools;
+
+namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
+{
+    class Common
+    {
+        public delegate int Calculate(int x, int y); // Used by: Ex001_UnderstandingDelegates_UsingADelegate
+
+        public int Add(int x, int y)
+        {
+            return x + y; // Used by: Ex001_UnderstandingDelegates_UsingADelegate
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y; // Used by: Ex001_UnderstandingDelegates_UsingADelegate
+        }
+    }
+}
---
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Ex001_UnderstandingDelegates_UsingADelegate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TommyTools;
+
+namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
+{
+    class Ex001_UnderstandingDelegates_UsingADelegate : BaseExercise
+    {
+        public override string Name { get; set; } = "UnderstandingDelegates - Using a delegate";
+
+        public override int Number { get; set; } = 1;
+
+        public Ex001_UnderstandingDelegates_UsingADelegate() { }
+
+        public override void Exec()
+        {
+            Common common = new Common();
+            Common.Calculate calc = common.Add;
+            Console.WriteLine(calc(3, 4)); // Displays 7
+            calc = common.Multiply;
+            Console.WriteLine(calc(3, 4)); // Displays 12
+        }
+    }
+}
