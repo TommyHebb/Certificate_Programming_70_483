a03ad36c9f2332bee027522d9c64148b18e9acc9
Tommy Hebb <tommyhebb@hotmail.com>
2019-02-27 17:27:08 +0100

    Added class Pub2 used for changed Obj 1_4 Ex008

--- Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Ex008_Events_UsingAnActionToExposeAnEvent.cs
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Ex008_Events_UsingAnActionToExposeAnEvent.cs
@@ -17,6 +17,11 @@ namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
             publisher.OnChange += () => Console.WriteLine("Event raised to method 1");
             publisher.OnChange += () => Console.WriteLine("Event raised to method 2");
             publisher.Raise();
+
+            Pub2 publisher2 = new Pub2();
+            publisher2.OnChange += () => Console.WriteLine("Event raised to method 3");
+            publisher2.OnChange += () => Console.WriteLine("Event raised to method 4");
+            publisher2.Raise();
         }
     }
 }
---
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Pub2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
+{
+    class Pub2
+    {
+        public event Action OnChange = delegate { };
+
+        public void Raise()
+        {
+            OnChange();
+        }
+    }
+}
