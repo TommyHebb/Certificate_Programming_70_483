e0d0429173a8336eb23f4b8fa9e92cb18d438dad
Tommy Hebb <tommyhebb@hotmail.com>
2019-02-27 13:28:35 +0100

    Extension exercise Obj 1_4 Ex002

--- Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Ex002_UnderstandingDelegates_AMulticastDelegate.cs
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Ex002_UnderstandingDelegates_AMulticastDelegate.cs
@@ -18,9 +18,12 @@ namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
             myDelegates += common.MethodTwo;
 
             myDelegates();
+            int invocationCount = myDelegates.GetInvocationList().GetLength(0);
+            Console.WriteLine("Number of methods added to the delegate: {0} ", invocationCount);
             // Displays: 
             // MethodOne
             // MethodTwo
+            // Number of methods added to the delegate: 2
         }
     }
 }
