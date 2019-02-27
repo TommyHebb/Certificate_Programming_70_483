9c9bcf9e238aba2fe8816c4d24b7905d25ec6f99
Tommy Hebb <tommyhebb@hotmail.com>
2019-02-27 17:09:19 +0100

    Added Obj 1_4 Ex008 using added class Pub1

---
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Ex008_Events_UsingAnActionToExposeAnEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TommyTools;
+
+namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
+{
+    class Ex008_Events_UsingAnActionToExposeAnEvent : BaseExercise
+    {
+        public override string Name { get; set; } = "Events - Using an Action to Expose an Event";
+
+        public override int Number { get; set; } = 8;
+
+        public Ex008_Events_UsingAnActionToExposeAnEvent() { }
+
+        public override void Exec()
+        {
+            Pub1 publisher = new Pub1();
+            publisher.OnChange += () => Console.WriteLine("Event raised to method 1");
+            publisher.OnChange += () => Console.WriteLine("Event raised to method 2");
+            publisher.Raise();
+        }
+    }
+}
---
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Pub1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
+{
+    class Pub1
+    {
+        public Action OnChange { get; set; }
+        public void Raise()
+        {
+            if (OnChange != null)
+            {
+                OnChange();
+            }
+        }
+    }
+}
