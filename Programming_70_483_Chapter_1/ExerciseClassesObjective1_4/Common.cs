b6d57151c36a862d251b0f3d448de3737def8de5
Tommy Hebb <tommyhebb@hotmail.com>
2019-02-27 15:31:22 +0100

    Added Obj 1_4 Ex005

--- Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Common.cs
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Common.cs
@@ -6,7 +6,8 @@ namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
 {
     class Common
     {
-        public delegate int Calculate(int x, int y); // Used by: Ex001_UnderstandingDelegates_UsingADelegate
+        public delegate int Calculate(int x, int y); // Used by: Ex001_UnderstandingDelegates_UsingADelegate, 
+        // Ex005_LambdaExpressions_LambdaExpressionToCreateADelegate and 
         public delegate void Delegate002(); // Used by: Ex002_UnderstandingDelegates_AMulticastDelegate
         public delegate TextWriter CovarianceDelegate(); // Used by: Ex003_UnderstandingDelegates_CovarianceWithDelegates
         public delegate void ContravarianceDelegate(StreamWriter writer); // Used by: Ex004_UnderstandingDelegates_ContravarianceWithDelegates
@@ -15,7 +16,6 @@ namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
         {
             return x + y; // Used by: Ex001_UnderstandingDelegates_UsingADelegate
         }
-
         public int Multiply(int x, int y)
         {
             return x * y; // Used by: Ex001_UnderstandingDelegates_UsingADelegate
@@ -25,7 +25,6 @@ namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
         {
             Console.WriteLine("MethodOne"); // Used by: Ex002_UnderstandingDelegates_AMulticastDelegate
         }
-
         public void MethodTwo()
         {
             Console.WriteLine("MethodTwo"); // Used by: Ex002_UnderstandingDelegates_AMulticastDelegate
---
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/Ex005_LambdaExpressions_LambdaExpressionToCreateADelegate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TommyTools;
+
+namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
+{
+    class Ex005_LambdaExpressions_LambdaExpressionToCreateADelegate : BaseExercise
+    {
+        public override string Name { get; set; } = "LambdaExpressions - Lambda Expression to create a Delegate";
+
+        public override int Number { get; set; } = 5;
+
+        public Ex005_LambdaExpressions_LambdaExpressionToCreateADelegate() { }
+
+        public override void Exec()
+        {
+            Common.Calculate calc = (x, y) => x + y;
+            Console.WriteLine(calc(3, 4)); // Displays 7
+            calc = (x, y) => x * y;
+            Console.WriteLine(calc(3, 4)); // Displays 12
+        }
+    }
+}
