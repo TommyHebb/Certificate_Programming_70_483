7cc33b394c3045afba68ec9179d8fbda0f015476
Tommy Hebb <tommyhebb@hotmail.com>
2019-03-13 15:36:03 +0100

    Added Obj 1_5 Ex002

---
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_5/Ex002_Exceptions_CatchingAFormatException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TommyTools;
+
+namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_5
+{
+    class Ex002_Exceptions_CatchingAFormatException : BaseExercise
+    {
+        public override string Name { get; set; } = "Exceptions - Catching a FormatException";
+
+        public override int Number { get; set; } = 2;
+
+        public Ex002_Exceptions_CatchingAFormatException() { }
+
+        public override void Exec()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter you first name: ");
+                string myString = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(myString))
+                {
+                    break;
+                }
+
+                try
+                {
+                    int myInteger = int.Parse(myString);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("{0} is not a valid number. Please try again", myString);
+                }
+            }
+        }
+    }
+}
