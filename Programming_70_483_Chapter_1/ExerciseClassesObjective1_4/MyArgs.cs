67764c1ea562c987174127cae2db68caf23ec864
Tommy Hebb <tommyhebb@hotmail.com>
2019-03-13 11:11:47 +0100

    little refactor - props before constructors

--- Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/MyArgs.cs
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_4/MyArgs.cs
@@ -4,11 +4,11 @@ namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_4
 {
     public class MyArgs : EventArgs
     {
+        public int Value { get; set; }
+
         public MyArgs(int value)
         {
             Value = value;
         }
-
-        public int Value { get; set; }
     }
 }
