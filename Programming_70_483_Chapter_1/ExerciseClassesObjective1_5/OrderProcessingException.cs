aecea9db6be04ffb417c780f6f1114713ae1046d
Tommy Hebb <tommyhebb@hotmail.com>
2019-04-10 14:36:58 +0200

    - Added OrderProcessingException to Obj 1_5

---
+++ Programming_70_483_Chapter_1/ExerciseClassesObjective1_5/OrderProcessingException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Programming_70_483_Chapter_1.ExerciseClassesObjective1_5
+{
+    [Serializable]
+    public class OrderProcessingException : Exception, ISerializable
+    {
+        public int OrderId { get; private set; }
+
+        public OrderProcessingException(int orderId)
+        {
+            OrderId = orderId;
+            this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+        }
+
+        public OrderProcessingException(int orderId, string message) : base(message)
+        {
+            OrderId = orderId;
+            this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+        }
+
+        public OrderProcessingException(int orderId, string message, Exception innerException) : base(message, innerException)
+        {
+            OrderId = orderId;
+            this.HelpLink = "http://www.mydomain.com/infoaboutexception";
+        }
+
+        protected OrderProcessingException(SerializationInfo info, StreamingContext context)
+        {
+            OrderId = (int)info.GetValue("OrderId", typeof(int));
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("OrderId", OrderId, typeof(int));
+        }
+    }
+}
